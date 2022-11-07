dbbae70f307ec45b151b27208aeee60bd1aa5b3e
Madoka Kato <maka7816@yahoo.co.jp>
2022-11-07 17:19:09 +0900

    トップページに戻る

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,15 @@
+import Link from 'next/link';
+
+
 export default function FirstPost() {
-    return <h1>最初の投稿</h1>;
-  }
+    return (
+    <>
+    <h1>最初の投稿</h1>
+      <h2>
+        <Link href="/">
+          <a>トップページに戻る</a>
+        </Link>
+      </h2>
+    </>
+  );
+}
