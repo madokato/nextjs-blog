533219c2625d4d99323c134f3a23987fe044790c
Madoka Kato <maka7816@yahoo.co.jp>
2022-11-08 12:00:43 +0900

    変更

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,45 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
+      
         <p>{/* ここに自己紹介を記述 */}</p>
         <p>
-          (これは Next.js のサンプルページです - これから{' '}
-          <a href="https://nextjs.org/learn">Next.js tutorial</a>{' '}
+          (これは Next.js のサンプルページです - これから{" "}
+          <a href="https://nextjs.org/learn">Next.js tutorial</a>{" "}
           のサイトにあるようなページを作っていきます。)
         </p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                {title}
+                <br />
+                {id}
+                <br />
+                {date}
+              </li>
+            ))}
+          </ul>
+        </section>
     </Layout>
   );
 }
